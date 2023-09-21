4d235d3e2243089fc8f13b1eb2c5be5ae01f4756
Kristina Romanova <hrusha@freenet.de>
2023-09-21 13:31:47 +0200

    homework_03

---
+++ homework_03.sql
@@ -0,0 +1,43 @@
+-- Вывести ко-во заказов, которое компания Speedy Express
+-- доставила в Brazil
+
+SELECT count(*) FROM [OrderDetails]
+JOIN Customers ON Orders.CustomerID = Customers.CustomerID
+JOIN Shippers ON Orders.ShipperID = Shippers.ShipperID
+JOIN Orders ON OrderDetails.OrderID= Orders.OrderID
+WHERE Shippers.ShipperName= 'Speedy Express'
+AND Customers.Country = 'Brazil'
+
+--Вывести ко-во поставщиков не из UK и не из China
+
+SELECT count(*) FROM [Suppliers]
+where not Country in ('UK','China')
+
+--Вывести среднюю/MAX/MIN стоимости и ко-во товаров из категорий 3 и 5
+
+SELECT AVG(Price)
+AS avg_price,
+MAX(Price)
+As max_price,
+MIN(Price)
+As min_price,
+count(*)FROM [Products]
+where CategoryID in (3,5)
+
+-- Вывести общую сумму проданных товаров
+
+SELECT sum(Products.Price) FROM [OrderDetails]
+join Products on OrderDetails.ProductID = Products.ProductID
+
+--Вывести ко-во стран, которые поставляют напитки
+
+SELECT count(Country) FROM [Products]
+join Suppliers on Products.SupplierID = Suppliers.SupplierID
+join Categories on Products.CategoryID= Categories.CategoryID
+where CategoryName = 'Beverages'
+
+--Вывести сумму, на которую было отправлено товаров клиентам в Germany
+
+SELECT sum(Price) FROM [Products]
+join Suppliers on Products.SupplierID= Suppliers.SupplierID
+where Country='Germany'
