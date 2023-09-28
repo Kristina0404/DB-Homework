46778377c25a48bd78ee65e8efa7ed5f9c60969b
Kristina Romanova <hrusha@freenet.de>
2023-09-28 14:58:18 +0200

    homework_04

--- homework_03.sql
+++ homework_03.sql
@@ -11,6 +11,7 @@ AND Customers.Country = 'Brazil'
 --Вывести ко-во поставщиков не из UK и не из China
 
 SELECT count(*) FROM [Suppliers]
+AS total_not_uk_china
 where not Country in ('UK','China')
 
 --Вывести среднюю/MAX/MIN стоимости и ко-во товаров из категорий 3 и 5
@@ -21,23 +22,33 @@ MAX(Price)
 As max_price,
 MIN(Price)
 As min_price,
-count(*)FROM [Products]
+count(*)
+AS total_products
+FROM [Products]
 where CategoryID in (3,5)
 
 -- Вывести общую сумму проданных товаров
 
-SELECT sum(Products.Price) FROM [OrderDetails]
+SELECT sum(Products.Price * OrderDetails.Quantity)
+AS total_order_cost
+FROM [OrderDetails]
 join Products on OrderDetails.ProductID = Products.ProductID
 
 --Вывести ко-во стран, которые поставляют напитки
 
-SELECT count(Country) FROM [Products]
+SELECT count(Distinct Country)
+As count_country FROM [Products]
 join Suppliers on Products.SupplierID = Suppliers.SupplierID
 join Categories on Products.CategoryID= Categories.CategoryID
 where CategoryName = 'Beverages'
 
 --Вывести сумму, на которую было отправлено товаров клиентам в Germany
 
-SELECT sum(Price) FROM [Products]
-join Suppliers on Products.SupplierID= Suppliers.SupplierID
+SELECT sum(Price * OrderDetails.Quantity)
+As total_sum
+FROM [OrderDetails]
+join Products on OrderDetails.ProductID = Products.ProductID
+join Orders on OrderDetails.OrderID = Orders.OrderID
+join Categories on OrderDetails.CategoryID = Categories.CategoryID
+//join Suppliers on Products.SupplierID= Suppliers.SupplierID
 where Country='Germany'
---
+++ homework_04.sql
@@ -0,0 +1,55 @@
+--1. Найти мин/стоимость товаров для каждой категории
+
+SELECT CategoryID,
+MIN(price) 
+As min_price FROM [Products]
+Group by CategoryID
+
+--2. Вывести ТОП-3 стран по количеству доставленных заказов
+
+SELECT
+Customers.Country,
+COUNT(*) AS total_orders
+FROM [Orders]
+JOIN Customers on Orders.CustomerID = Customers.CustomerID
+Group By Customers.CustomerID
+ORDER BY total_orders DESC
+LIMIT 3
+
+--3. Вывести названия категорий, в которых более 10 товаров
+
+SELECT CategoryName,
+count(*) AS total_products
+ FROM [Products]
+ join Categories on Products.CategoryID= Categories.CategoryID
+ Group by Categories.CategoryID
+ Having total_products >10
+
+ --4. Очистить тел/номер поставщикам из USA
+
+Update Suppliers
+SET
+Phone = ' '
+Where Country = 'USA'
+
+--5. Вывести имена и фамилии (и ко-во заказов) менеджеров, у которых ко-во заказов менее 15
+
+SELECT FirstName,LastName,
+count(*)
+AS total_orders
+FROM [Orders]
+join Employees on Orders.EmployeeID = Employees.EmployeeID
+Group by Employees.EmployeeID
+Having total_orders <15
+Order by total_orders DESC
+
+--6. Вывести товар, который находится на втором месте по ко-ву заказов
+
+SELECT Products.ProductName,
+SUM(OrderDetails.Quantity)
+AS sold_count
+From OrderDetails
+join Products on OrderDetails.ProductID = Products.ProductID
+GROUP BY OrderDetails.ProductID
+ORDER BY sold_count DESC
+Limit 1
